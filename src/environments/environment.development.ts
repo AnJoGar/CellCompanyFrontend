f6a6471c176665f64bc50b83e3e34141d04c490d
AnjoGar <andjor2020@gmail.com>
2026-01-03 17:42:59 -0500

    jkjxxkdd

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-  // endpoint: 'https://apicredito2-ximx.onrender.com/api/'
-    endpoint: 'http://192.168.100.13:7166/api/'
+   endpoint: 'https://apicredito2-ximx.onrender.com/api/'
+  //  endpoint: 'http://192.168.100.13:7166/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    endpoint: 'http://192.168.100.13:7166/api/'
+     endpoint: 'https://apicredito2-ximx.onrender.com/api/'
 };
